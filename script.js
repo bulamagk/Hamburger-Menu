a134092f1bf4d3565275b1d45682b1e3d6ca2db6
= <=>
2022-10-07 15:46:23 +0100

    Added Modal Functionality

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const menuIcons = document.querySelector(".menuIcons");
 const backdrop = document.querySelector(".backdrop");
+const contact = document.querySelector(".contact");
+const closeBtn = document.querySelector(".close");
+const modalBox = document.querySelector(".modal");
 const ul = document.querySelector("ul");
 
-menuIcons.addEventListener("click", () => toggleMenu());
+menuIcons.addEventListener("click", () => toggleModal && toggleMenu());
 backdrop.addEventListener("click", () => toggleMenu());
+contact.addEventListener("click", () => toggleModal && toggleModal());
+closeBtn.addEventListener("click", () => toggleModal && toggleModal());
+
+const toggleModal = () => {
+  modalBox.classList.toggle("active");
+};
 
 const toggleMenu = () => {
   ul.classList.toggle("active");
